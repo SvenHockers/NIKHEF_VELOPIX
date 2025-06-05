12691aaaa0fff5b2a00f00ec06df14d53b5cbf9d
SvenHockers <90685770+SvenHockers@users.noreply.github.com>
2025-06-05 23:34:15 +0200

    NikHef Integration

--- VeloPix_exe.sh
+++ VeloPix_exe.sh
@@ -12,6 +12,7 @@ pip install -r velopix/requirements.txt
 # Install custom velopix wheel
 pip install velopix/velopix-0.7.7-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 
+mkdir -p "velopix/output"
 # run the Python analysis script
 for cfg in configurations/*.json; do
     python3 velopix/VeloPix_HyperParamHandler.py --config "$cfg"
