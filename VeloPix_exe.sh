a14daef742a4adcbb27d3425274fb53d31cac37c
SvenHockers <90685770+SvenHockers@users.noreply.github.com>
2025-06-06 18:52:58 +0200

    Start V2 rollout

--- VeloPix_exe.sh
+++ VeloPix_exe.sh
@@ -7,10 +7,7 @@ tar xzf velopix.tar.gz
 # build env 
 python3 -m venv env
 source env/bin/activate
-pip install -r velopix/requirements.txt
-
-# Install custom velopix wheel
-pip install velopix/velopix-0.7.7-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+pip install velopix
 
 mkdir -p "velopix/output"
 # run the Python analysis script
